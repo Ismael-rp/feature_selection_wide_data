54ae0dcb5b2448e8a9fc94eee654e24a70f7ae01
ismael-rp <ismaelrp@ubu.es>
2021-05-04 11:00:09 +0200

    Reordered functions

--- example.R
+++ example.R
@@ -8,11 +8,11 @@ data = iris[1:100,]
 
 data = partitionDataTag(data)
 
-fSelection_filter_chi_squared(data)
-fSelection_filter_information_gain(data)
-fSelection_filter_relief(data)
-fSelection_filter_anova(data)
-fSelection_random_forest_importance(data)
 fSelection_t_test(data)
+fSelection.filter.anova(data)
+fSelection.filter.chi.squared(data)
+fSelection.filter.information.gain(data)
+fSelection_random_forest_importance(data)
+fSelection_filter_relief(data)
 fSelection_svm_rfe(data)
 
